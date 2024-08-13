f758c1c4d5679c378dbf5d01cd32b95bf2530396
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-13 14:13:10 +0530

    Update PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import { Route, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
-const PrivateRoute = ({ element: Component, isAuthenticated, ...rest }) => (
-  <Route
-    {...rest}
-    element={isAuthenticated ? Component : <Navigate to="/login" />}
-  />
-);
+const PrivateRoute = ({ element: Component, isAuthenticated, ...rest }) => {
+  return isAuthenticated ? Component : <Navigate to="/login" />;
+};
 
 export default PrivateRoute;
